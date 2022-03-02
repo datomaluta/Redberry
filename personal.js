ed18e7742b56d71536680ca20177cd2c896facf6
datomaluta <datomaluta@gmail.com>
2022-03-02 18:14:16 +0400

    before start functionality

---
+++ personal.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const nextBtn = document.querySelector(".next-btn");
+// console.log(nextBtn);
+
+nextBtn.addEventListener("click", function () {
+  window.location.href = "http://127.0.0.1:5500/pages/skills.html";
+  `http://127.0.0.1:5500/pages/page${curr}`;
+});
